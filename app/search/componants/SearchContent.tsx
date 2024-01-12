81b4039521e15dd23ee8c0cd7129a16af3538776
Arpit Bansal <arpitbansal2304@gmail.com>
2024-01-12 16:47:12 +0530

    onPlay fix

--- app/search/componants/SearchContent.tsx
+++ app/search/componants/SearchContent.tsx
@@ -10,6 +10,7 @@ interface SearchContentProps {
 const SearchContent:React.FC<SearchContentProps> = ({
     songs
 }) => {
+    const onPlay=useOnPlay(songs);
     if(songs.length===0){
         return(
             <div
@@ -26,7 +27,6 @@ const SearchContent:React.FC<SearchContentProps> = ({
             </div>
         )
     }
-    const onPlay=useOnPlay(songs);
     return (
     <div className="flex flex-col gap-y-2 w-full px-6">
         {songs.map((item) => (
